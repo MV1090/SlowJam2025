6cfa24b54d120e8ba58d34d42f44d2d22f4d68d9
Tyson H. <56943752+tysonkh@users.noreply.github.com>
2025-05-26 14:36:41 -0600

    Add world object pooling and level manager

--- Assets/_Scripts/Obstacles/WorldObstacle.cs
+++ Assets/_Scripts/Obstacles/WorldObstacle.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class WorldObstacle : MonoBehaviour
 {
-    public float moveSpeed = 100.0f;
+    private float moveSpeed = 10.0f;
+    private float deactivateZPoint = -10.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,11 +14,23 @@ public class WorldObstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.z < -10.0f)
+        if(transform.position.z < deactivateZPoint)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, 10.0f);
+            gameObject.SetActive(false);
         }
 
-        transform.Translate( (Vector3.forward * moveSpeed) * Time.deltaTime);
+        // Move towards the 0 point
+        transform.Translate( -(Vector3.forward * moveSpeed) * Time.deltaTime);
+        
+    }
+
+    public void SetMoveSpeed(float newMoveSpeed)
+    {
+        moveSpeed = newMoveSpeed;
+    }
+
+    public void SetDeactivatePoint(float newZ)
+    {
+        deactivateZPoint = newZ;
     }
 }
