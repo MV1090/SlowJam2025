b3cb04f98c337101814eb48ac42f81d49198671c
Mark <mark.vale@torontofilmschool.ca>
2025-06-05 16:40:52 -0600

    Update settings Menu

--- Assets/_Scripts/Menus Scripts/SettingMenu.cs
+++ Assets/_Scripts/Menus Scripts/SettingMenu.cs
@@ -1,11 +1,34 @@
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingMenu : BaseMenu
 {
+
+    [SerializeField] Slider sfxVolume;
+    [SerializeField] Slider musicVolume;
+    [SerializeField] AudioMixer audioMixer;
     public override void InitState(MenuManager ctx)
     {
         base.InitState(ctx);
         state = MenuManager.MenuStates.SettingsMenu;
+
+        if (sfxVolume)
+        {
+            sfxVolume.onValueChanged.AddListener((value) => OnVolumeChanged(value, "SFXVolume"));
+            float newValue;
+            audioMixer.GetFloat("SFXVolume", out newValue);
+            sfxVolume.value = newValue + 80;
+        }
+
+        if (musicVolume)
+        {
+            musicVolume.onValueChanged.AddListener((value) => OnVolumeChanged(value, "MusicVolume"));
+            float newValue;
+            audioMixer.GetFloat("MusicVolume", out newValue);
+            musicVolume.value = newValue + 80;
+        }
+
     }
 
     public override void EnterState()
@@ -19,4 +42,9 @@ public class SettingMenu : BaseMenu
         base.ExitState();
         Time.timeScale = 1.0f;
     }   
+
+    void OnVolumeChanged(float value, string sliderName)
+    {
+        audioMixer.SetFloat(sliderName, value -80);
+    }
 }
