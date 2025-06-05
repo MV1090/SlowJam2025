d23e92722fb66e6d52ce7c5938437a7848898c29
Mark <mark.vale@torontofilmschool.ca>
2025-06-05 10:01:34 -0600

    Updated scene to work with menu system

--- Assets/_Scripts/Player/Reticle.cs
+++ Assets/_Scripts/Player/Reticle.cs
@@ -5,6 +5,7 @@ public class MouseReticle : MonoBehaviour
 {
     public Camera _camera;
     private SpriteRenderer sr;
+    public float zOffset;
 
     private void Start()
     {
@@ -15,7 +16,7 @@ public class MouseReticle : MonoBehaviour
     {
         Vector2 mousePosition = Mouse.current.position.ReadValue();
 
-        Vector3 worldMousePosition = _camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, _camera.nearClipPlane));
+        Vector3 worldMousePosition = _camera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, zOffset));
         
         transform.position = worldMousePosition;               
     }
